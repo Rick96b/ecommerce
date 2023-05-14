febb894c7900946be1be4b4f28d107601864bbce
Oleg <olegkorobeinikov21@gmail.com>
2023-05-14 22:04:25 +0500

    mobile header completed

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -5,21 +5,29 @@ import styles from './Header.module.scss';
 import BaseButton from 'shared/ui/BaseButton';
 import useOutsideClick from './model/useOutsideClick';
 import OpenMenuButton from './ui/OpenMenuButton';
+import MediaQuery from "react-responsive";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
-  const ref = useOutsideClick(toggleOpen)
+  const ref = useOutsideClick(closeModal)
 
   function toggleOpen() {
     setOpen(!open);
   }
 
+  function closeModal() {
+    if(open) setOpen(false);
+  }
+
   return (
     <header className={styles.header}  ref={ref}>
-      <div className={styles.headerContainer}>
-        <OpenMenuButton toggleMenu={toggleOpen}/>
-      </div>
-      <BurgerMenu isOpen={open}/>
+      <MediaQuery maxWidth={768}>
+        <div className={styles.headerContainer}>
+          <span className={styles.headerOpenMenuButton}><OpenMenuButton toggleMenu={toggleOpen}/></span>
+          <span className={styles.headerLogoText}>Valentina<br /> Guest House</span>
+        </div>
+        <BurgerMenu isOpen={open}/>
+      </MediaQuery>
     </header>
   )
 }
--- src/widgets/header/ui/BurgerMenu/index.tsx
+++ src/widgets/header/ui/BurgerMenu/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames';
+import { NavLink } from "react-router-dom";
 
 import styles from './BurgerMenu.module.scss';
 
@@ -10,8 +11,17 @@ interface Props {
 const BurgerMenu: React.FC<Props> = ({isOpen}) => {
   return (
     <div className={classNames(styles.menu, isOpen ? styles.menuOpened : '')}>
-        <ul className={styles.menu__box}>
-            <li className={styles.menu__item}>Главная</li>
+        <ul className={styles.menu__box}>       
+            <li className={styles.menu__item}>
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? styles.activeLink : ""
+              }
+            >
+              Главная
+            </NavLink>
+            </li>
             <li className={styles.menu__item}>Номера и цены</li>
             <li className={styles.menu__item}>Статьи</li>
             <li className={styles.menu__item}>Информация</li>
